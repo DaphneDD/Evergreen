c62a3d45177b2d7a9362d88bbe865a6e2bad7f3f
LAPTOP-25IBET6C\dongx <dongxq12@gmail.com>
2020-08-15 20:04:52 -0700

    queries

--- DB/fake_data.sql
+++ DB/fake_data.sql
@@ -1,8 +1,39 @@
+USE `evergreen`;
 
 LOCK TABLES `candidates` WRITE, `tests` WRITE, `test_centers` WRITE, `exams` WRITE, `clients` WRITE;
 
-INSERT INTO `candidates` (first_name, last_name, id_type, id_number) VALUES
+INSERT INTO `candidates` (`first_name`, `last_name`, `id_type`, `id_number`) VALUES
 ('Amanda', 'Johnson', 'drivers_license', 'DL109213'),
+('Lilian', 'Abu', 'drivers_license', 'DL0291242'),
+('Julian', 'Wang', 'drivers_license', 'DL2901483'),
+('Fei', 'Xie', 'passport', 'A9201923'),
+('Fatima', 'Said', 'drivers_license', 'DL109213'),
 ('Brent', 'Ho', 'passport', 'Z209H892D');
+
+INSERT INTO `test_centers` (`name`, `address`, `number_of_stations`) VALUES
+('Maple Grove', '200 California Dr, Maple Grove', 4),
+('Apple Creek', '12 Forest Ave, Apple Creek', 4);
+
+INSERT INTO `clients` (`name`, `address`, `phone_number`, `creation_time`) VALUE
+('Drawing Circle Society', '1 Circular Dr, Sphere County', '1-672-132-1241', '2020-08-10'),
+('Catfish Fishing Association', '20 Riverside Ave, River City', '1-230-185-9302', '2020-08-12');
+
+INSERT INTO `exams` (`name`, `duration`, `description`, `client_id`) VALUES
+('Drawing Perfect Circle', '01:30:00', 'drawing two perfect circles using a mouse', 1),
+('Drawing Concentric Circles', '06:00:00', 'drawing as many concentric circles as possible using a mouse', 1),
+('Catfish Fishing License Exam', '03:00:00', 'Licensing exam to test candidate\'s knowledge of catfish fishing', 2),
+('Catfish Tools License Exam', '10:00:00', 'Licensing exam to make legal tools for catfish fishing', 2);
+
+INSERT INTO `tests` (`exam_id`, `candidate_id`, `date`, `start_time`, `test_center_id`, `status`) VALUES
+(1, 1, '2020-10-13', '08:00:00', 1, 'Registered'),
+(2, 6, '2020-10-13', '09:30:00', 1, 'Registered'),
+(3, 4, '2020-10-13', '10:00:00', 1, 'Registered'),
+(4, 3, '2020-10-13', '08:00:00', 1, 'Registered'),
+(1, 2, '2020-10-13', '13:00:00', 1, 'Registered'),
+(3, 5, '2020-10-13', '13:00:00', 1, 'Registered'),
+(2, 1, '2020-10-13', '12:00:00', 1, 'Registered'),
+(2, 5, '2020-10-19', '08:00:00', 2, 'Registered'),
+(4, 1, '2020-10-19', '08:00:00', 2, 'Registered'),
+(1, 4, '2020-10-13', '11:00:00', 2, 'Registered');
 
 UNLOCK TABLES;
--- DB/init_DB.sql
+++ DB/init_DB.sql
@@ -1,19 +1,22 @@
+-- create database evergreen
+DROP DATABASE IF EXISTS `evergreen`;
+CREATE DATABASE `evergreen`;
+USE `evergreen`;
+
 -- candidates table
 -- stores names and identifications
-DROP TABLE IF EXISTS `candidates`;
-CREATE TABLE 'candidates' (
-    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `candidates` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
     `first_name` VARCHAR(255),
     `last_name` VARCHAR(255),
-    'id_type' ENUM('passport', 'drivers_license') NOT NULL,
-    "id_number" varchar(255) NOT NULL
+    `id_type` ENUM('passport', 'drivers_license') NOT NULL,
+    `id_number` varchar(255) NOT NULL
 );
 
 -- test_centers table
 -- info about the test centers
-DROP TABLE IF EXISTS `test_centers`;
 CREATE TABLE `test_centers` (
-    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `address` VARCHAR(255) NOT NULL,
     `number_of_stations` TINYINT UNSIGNED NOT NULL
@@ -21,38 +24,44 @@ CREATE TABLE `test_centers` (
 
 -- clients table
 -- stores clients info
-DROP TABLE IF EXISTS `clients`;
-CREATE TABLE 'clients' (
-    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `clients` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `address` VARCHAR(255),
     `phone_number` VARCHAR(20),
     `creation_time` DATE
 );
 
+
+
 -- exams table
 -- stores exams info
-DROP TABLE IF EXISTS `exams`;
-CREATE TABLE 'exams' (
-    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `exams` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `duration` TIME NOT NULL,
-    `phone_number` VARCHAR(20),
-    `creation_time` DATE,
-    `client_id` INT(11) FOREIGN KEY REFERENCES `clients` (`id`)
+    `description` TEXT,
+    `client_id` INT NOT NULL,
+    FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
 );
 
 -- tests table
 -- stores info of each test
-DROP TABLE IF EXISTS `tests`;
-CREATE TABLE 'tests' (
-    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
-    `exam_id` INT(11) FOREIGN KEY REFERENCES `exams` (`id`),
-    `candidate_id` INT(11) FOREIGN KEY REFERENCES `candidates` (`id`),
+CREATE TABLE `tests` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `exam_id` INT NOT NULL,
+    `candidate_id` INT NOT NULL,
     `date` DATE NOT NULL,
     `start_time` TIME NOT NULL,
-    `test_center_id` INT(11) FOREIGN KEY REFERENCES `test_centers` (`id`),
-    `status` ENUM(`Registered`, `Canceled`, `Checked in`, `In Progress`, `Completed`)
+    `test_center_id` INT NOT NULL,
+    `status` ENUM('Registered', 'Canceled', 'Checked in', 'In Progress', 'Completed'),
+    FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`),
+    FOREIGN KEY (`candidate_id`) REFERENCES `candidates` (`id`),
+	FOREIGN KEY (`test_center_id`) REFERENCES `test_centers` (`id`)
 );
 
+-- create a user for server
+DROP USER IF EXISTS 'evergreen_local_server'@'localhost';
+CREATE USER 'evergreen_local_server'@'localhost' IDENTIFIED BY 'evergreen_local_server';
+GRANT SELECT, INSERT, DELETE, UPDATE ON `evergreen`.* TO 'evergreen_local_server'@'localhost';
 
