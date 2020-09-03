e1ca8c012a2b96967999a9e71af83bc57cd6f3a2
LAPTOP-25IBET6C\dongx <dongxq12@gmail.com>
2020-09-02 23:22:35 -0700

    class-based views

--- server/tests/urls.py
+++ server/tests/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('test_list', views.test_list, name='test_list'),
-    path('candidate_list', views.candidate_list, name='candidate_list'),
-    path('candidate_detail/<int:pk>/', views.candidate_detail, name='candidate_detail'),
+    #path('candidate_list', views.candidate_list, name='candidate_list'),
+    path('candidate_list', views.CandidateListAPIView.as_view(), name='candidate_list'),
+    path('candidate_generic/<int:id>/', views.GenericAPIView.as_view(), name='candidate_generic'),
+    #path('candidate_detail/<int:pk>/', views.candidate_detail, name='candidate_detail'),
+    path('candidate_detail/<int:id>/', views.CandidateDetailAPIView.as_view(), name='candidate_detail'),
 ]
--- server/tests/views.py
+++ server/tests/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-from rest_framework.parsers import JSONParser
+#from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from .models import Candidate
 from .serializers import CandidateSerializer
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics
+from rest_framework import mixins
+
+
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = CandidateSerializer
+    queryset = Candidate.objects.all()
+    lookup_field = 'id'
+    
+    def get(self, request, id = None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
 
 def test_list(request):
     if request.method == 'GET':
@@ -15,6 +39,46 @@ def test_list(request):
         data = {"message": "This route only handles get request"}
     return JsonResponse(data)
 
+
+class CandidateListAPIView(APIView):
+    def get(self, request):
+        candidates = Candidate.objects.all()
+        serializer = CandidateSerializer(candidates, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CandidateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CandidateDetailAPIView(APIView):
+    def get_object(self, id):
+        try:
+            return Candidate.objects.get(id=id)
+        except Candidate.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        candidate = self.get_object(id)
+        serializer = CandidateSerializer(candidate)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        candidate = self.get_object(id)
+        serializer = CandidateSerializer(candidate, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        candidate = self.get_object(id)
+        candidate.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST'])
 def candidate_list(request):
