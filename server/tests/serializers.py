af5cfc5c58e772225062e5c38b3b909c3f9b5a57
LAPTOP-25IBET6C\dongx <dongxq12@gmail.com>
2020-09-05 23:02:42 -0700

    fixed duration name

--- server/tests/serializers.py
+++ server/tests/serializers.py
@@ -4,17 +4,10 @@ from .models import Candidate, Test
 class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
-        #fields = ['id', 'first_name', 'last_name', 'id_type', 'id_number']
         fields = '__all__'
 
 class TestSerializer(serializers.ModelSerializer):
-    exam = serializers.SlugRelatedField(
-        many=False,
-        read_only=True,
-        slug_field='duration'
-    )
-    #duration = serializers.TimeField('exam')
+    duration = serializers.TimeField(source='exam.duration')
     class Meta:
         model = Test
-        fields = ['id', 'exam_id','start_time', 'exam', 'status']
-        #fields = ['id', 'exam_id','start_time', 'duration', 'status']
+        fields = ['id', 'exam_id','start_time', 'duration', 'status']
